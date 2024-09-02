da8f7fafa0942ead2d2fc076dd9612c78aee3df1
MitchellScottHenke <henkester@gmail.com>
2024-09-01 15:17:06 -1000

    Starting the to do project

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
-// Making a to do app via rust
-// Following a medium page: https://medium.com/@goelpulkit43/build-a-todo-list-application-in-rust-c2cbe3f2938b
+// Making a todo app via rust
+// Following a code camp: https://www.freecodecamp.org/news/how-to-build-a-to-do-app-with-rust/
 
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashMap;
+
+struct Todo {
+    // use rust built-in HashMap to store key - value pairs
+    map: HashMap<String, bool>,
+}
+
+impl Todo {
+    fn insert(&mut self, key: String) {
+        //insert a new item into our map
+        // we pass true as value
+        self.map.insert(key, true);
+    }
+}
+
+fn main(){
+    let action = std::env::args().nth(1).expect("Please specify an action");
+    let item = std::env::args().nth(2).expect("Please specify an item");
+
+    println!("{:?}, {:?}", action, item);
 }

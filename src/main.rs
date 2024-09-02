365254d2ea0ee4064eac220ce065d46860f2b347
MitchellScottHenke <henkester@gmail.com>
2024-09-01 14:51:55 -1000

    Starting the to do project

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+// Making a to do app via rust
+// Following a medium page: https://medium.com/@goelpulkit43/build-a-todo-list-application-in-rust-c2cbe3f2938b
+
+fn main() {
+    println!("Hello, world!");
+}

33a4fd8f856c15e316a074e7978f1cb039e05f21
MitchellScottHenke <henkester@gmail.com>
2024-09-01 16:07:00 -1000

    Can now edit the db to complete an item

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,15 @@ fn main() {
             Ok(_) => println!("todo saved"),
             Err(why) => println!("An error occurred: {}", why),
         }
-    };
+    } else if action == "complete" {
+        match todo.complete(&item) {
+            None => println!("'{}' is not present in the list", item),
+            Some(_) => match todo.save() {
+                Ok(_) => println!("todo saved"),
+                Err(why) => println!("An error occurred: {}", why),
+            }
+        }
+    }
 }
 
 struct Todo {
@@ -56,5 +64,12 @@ impl Todo {
             content.push_str(&record)
         }
         std::fs::write("db.txt", content)
+    }
+
+    fn complete(&mut self, key: &String) -> Option<()> {
+        match self.map.get_mut(key) {
+            Some(v) => Some(*v = false),
+            None => None,
+        }
     }
 }

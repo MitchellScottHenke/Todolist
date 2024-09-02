4b061c8770fba9a5458ad9380aace5df66ad9bed
MitchellScottHenke <henkester@gmail.com>
2024-09-01 15:30:08 -1000

    first trial run

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,24 @@
 
 use std::collections::HashMap;
 
+fn main(){
+    let action = std::env::args().nth(1).expect("Please specify an action");
+    let item = std::env::args().nth(2).expect("Please specify an item");
+
+    println!("{:?}, {:?}", action, item);
+
+    let mut todo = Todo {
+        map: HashMap::new(),
+    };
+    if action == "add" {
+        todo.insert(item);
+        match todo.save() {
+            Ok(_) => println!("todo saved"),
+            Err(why) => println!("An error occurred: {}" why),
+        }
+    }
+}
+
 struct Todo {
     // use rust built-in HashMap to store key - value pairs
     map: HashMap<String, bool>,
@@ -14,11 +32,13 @@ impl Todo {
         // we pass true as value
         self.map.insert(key, true);
     }
-}
 
-fn main(){
-    let action = std::env::args().nth(1).expect("Please specify an action");
-    let item = std::env::args().nth(2).expect("Please specify an item");
-
-    println!("{:?}, {:?}", action, item);
+    fn save(self) -> Result<(), std::io::Error> {
+        let mut content = String::new();
+        for (k, v) in self.map {
+            let record = format!("{}\t{}\n", k, v);
+            content.push_str(&record)
+        }
+        std::fs::write("db.txt", content)
+    }
 }
